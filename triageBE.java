09d667bb241e284c0736d70e4b95ff68c862f8df
ytquek <youtingquek@gmail.com>
2024-03-08 01:21:10 +0800

    extension for Get Method

--- triageBE.java
+++ triageBE.java
@@ -1,5 +1,22 @@
-public class triageBE {
+import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class triageBE extends HttpServlet {
     public static void main(String[] args) {
         System.out.println("Hello, World! This is triageBE and its really cool!"); 
     }
+
+    public void doGet(HttpServletRequest request,
+                    HttpServletResponse response)
+      throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+
+        out.print("Hello");
+
+        // ... pseudocode
+       
+
 }
